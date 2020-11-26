c71ace3e316ad01d2123d52d2190c6dcd9f133ef
izhakmo <izhakmo@post.bgu.ac.il>
2020-11-26 21:59:53 +0200

    opened bucket successfully

--- src/main/java/ocrdemo/local_application.java
+++ src/main/java/ocrdemo/local_application.java
@@ -1,16 +1,40 @@
 package ocrdemo;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
-
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Random;
 
 public class local_application {
 
-    private static S3Client s3;
-    public static void main(String[] args){
-        Region region = Region.US_EAST_1;
-        S3Client s3 = S3Client.builder()
-                .region(region)
-                .build();
+    private static AmazonS3 s3;
+
+    public static void main(String[] args) throws IOException {
+        s3 = AmazonS3ClientBuilder.defaultClient();
+
+        Bucket buck =  s3.createBucket("moshehagever");
+
+
+//        // Upload a text string as a new object.
+        s3.putObject("moshehagever", "stringObjKeyName", "Uploaded String Object");
+
+
+        // Upload a file as a new object with ContentType and title specified.
+        PutObjectRequest request = new PutObjectRequest("moshehagever", "fileObjKeyName", new File("C:\\Users\\izhak\\IdeaProjects\\text.images.txt"));
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType("plain/text");
+        metadata.addUserMetadata("title", "someTitle");
+        request.setMetadata(metadata);
+        s3.putObject(request);
+
+
     }
+
 }
